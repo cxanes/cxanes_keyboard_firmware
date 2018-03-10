f3b0610d310260f9083cf21df01b1bf6ce9d44e9
Frank Chang <frank.nevermind@gamil.com>
2018-03-11 01:24:41 +0800

    Add new board Bface60

---
+++ qmk_firmware/keyboards/bface60/bface60.c
@@ -0,0 +1,71 @@
+/*
+Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "bface60.h"
+#ifdef BACKLIGHT_ENABLE
+#include "backlight.h"
+#endif
+#ifdef RGBLIGHT_ENABLE
+#include "rgblight.h"
+#endif
+
+#include <avr/pgmspace.h>
+
+#include "action_layer.h"
+#include "i2c.h"
+#include "quantum.h"
+
+#define CAPSLOCK_LED (1 << 1)
+
+#ifdef RGBLIGHT_ENABLE
+extern rgblight_config_t rgblight_config;
+
+void rgblight_set(void) {
+    if (!rgblight_config.enable) {
+        for (uint8_t i = 0; i < RGBLED_NUM; i++) {
+            led[i].r = 0;
+            led[i].g = 0;
+            led[i].b = 0;
+        }
+    }
+
+    i2c_init();
+    i2c_send(0xb0, (uint8_t*)led, 3 * RGBLED_NUM);
+}
+#endif
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+}
+
+void backlight_init_ports(void) {
+    DDRD |= CAPSLOCK_LED;
+    PORTD |= CAPSLOCK_LED;
+}
+
+void backlight_set(uint8_t level) {
+}
+
+// @Override
+// turn LEDs on and off depending on USB caps/num/scroll lock states.
+void led_set_user(uint8_t usb_led) {
+    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+        PORTD |= CAPSLOCK_LED;
+    } else {
+        PORTD &= ~CAPSLOCK_LED;
+    }
+}
---
+++ qmk_firmware/keyboards/bface60/readme.md
@@ -0,0 +1,19 @@
+B.fake
+========
+
+A 60% keyboard with RGB
+
+Keyboard Maintainer: QMK Community
+Hardware Supported: Bface60 PCB
+Hardware Availability: https://shopee.tw/Bface60-PCB-i.7412224.268641687
+
+Make example for this keyboard (after setting up your build environment):
+
+    make bface60:default
+
+You could enter the bootloader mode manually
+by plugging the keyboard in while holding the bootloader key. If you did this
+correctly the LEDs will blink and you'll be able to flash your firmware.
+The bootloader key is `L_Ctrl` for this board (bottom left key of the keyboard).
+
+See [build environment setup](https://docs.qmk.fm/build_environment_setup.html) then the [make instructions](https://docs.qmk.fm/make_instructions.html) for more information.
